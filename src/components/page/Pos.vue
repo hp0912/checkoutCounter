c3a36dc3492e06b52437eb62177a3030de2ea4be
hp0912 <809211365@qq.com>
2018-05-06 21:33:46 +0800

    2018/05/06 21:35

---
+++ src/components/page/Pos.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="pos">
+    <div>
+      <el-row>
+        <el-col :span="7" class="pos-container">
+          AAAAAAAAAAAAAAAAA
+        </el-col>
+        <el-col :span="17" class="pos-container">
+          BBBBBBBBBBBBBBBBB
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pos',
+  mounted () {
+    var elements = document.getElementsByClassName('pos-container')
+    var containerHeight = document.body.clientHeight
+    for (var i = 0, len = elements.length; i < len; i++) {
+      elements[i].style.height = containerHeight + 'px'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.pos-container {
+  border-right: 1px solid #909399;
+}
+</style>
